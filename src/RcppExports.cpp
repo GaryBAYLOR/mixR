cb50236b8f74cc93c07f3f69e58b7df357a9e6b5
Youjiao (Gary) Yu <jiaoisjiao@gmail.com>
2022-01-02 23:03:00 -0600

    Fixing issue 22

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rcmean
 NumericVector rcmean(NumericMatrix x, int margin);
 RcppExport SEXP _mixR_rcmean(SEXP xSEXP, SEXP marginSEXP) {
